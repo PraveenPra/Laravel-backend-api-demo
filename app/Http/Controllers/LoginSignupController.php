37a85596c41d7c54c6a198543a4012e5a2fd1957
Manikanta <prvp6501@gmail.com>
2021-08-24 11:21:05 +0530

    made a controller and model for signup and login. (works with postman)

---
+++ app/Http/Controllers/LoginSignupController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
+class LoginSignupController extends Controller
+{
+    
+    private $status_code = 200;
+    
+    public function userSignUp(Request $req){
+
+       
+        // ------------ [ User Signup ] -------------------
+
+        //form field validation
+        $validator = Validator::make($req->all(), [
+            "name"     => "required",
+            "email"    => "required|email",
+            "password" => "required", 
+            ]);
+
+        if($validator->fails()) {
+        return response()->json(["status" => "failed", "message" => "validation_error", "errors" => $validator->errors()]);
+        }
+
+        //if using same email to register again, registry fail
+        $user_status =  User::where("email", $req->email)->first();
+
+        if(!is_null($user_status)) {
+           return response()->json(["status" => "failed", "success" => false, "message" => "Whoops! email already registered"]);
+        }
+
+        //save the form data in the database
+        $UserDetails= new User;
+        $UserDetails->name=$req->name;
+        $UserDetails->email=$req->email;
+        $UserDetails->password=md5($req->password);
+        $UserDetails->phone=$req->phone;
+        $UserDetails->role=$req->role;
+        $result= $UserDetails->save();
+        if($result){
+         // return [$UserDetails];
+            return response()->json(["message" => "Registration completed successfully", "data" => $UserDetails]);
+        }
+       else{
+        //    return "failed to save";
+        return response()->json(["message" => "failed to register"]);
+    }
+    }
+    
+    // ------------ [ User Login ] -------------------
+    public function userLogin(Request $request) {
+
+        //form validation
+        $validator = Validator::make($request->all(),
+            [
+                "email"   => "required|email",
+                "password"=> "required"
+            ]
+        );
+
+        if($validator->fails()) {
+            return response()->json(["status" => "failed", "validation_error" => $validator->errors()]);
+        }
+
+
+        // check if entered email exists in db
+        $email_status = User::where("email", $request->email)->first();
+
+
+        // if email exists then we will check password for the same email
+
+        if(!is_null($email_status)) {
+            $password_status = User::where("email", $request->email)->where("password", md5($request->password))->first();
+
+            // if password is correct
+            if(!is_null($password_status)) {
+                $user = $this->userDetail($request->email);
+
+                return response()->json(["status" => $this->status_code, "success" => true, "message" => "You have logged in successfully", "data" => $user]);
+            }
+
+            else {
+                return response()->json(["status" => "failed", "success" => false, "message" => "Unable to login. Incorrect password."]);
+            }
+        }
+
+        
+        else {
+            return response()->json(["status" => "failed", "success" => false, "message" => "Unable to login. Email doesn't exist."]);
+        }
+    }
+
+    // ------------------ [ User Details ] ---------------------
+    public function userDetail($email) {
+        $user = array();
+        if($email != "") {
+            $user = User::where("email", $email)->first();
+            return $user;
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginSignupController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post("signup",[LoginSignupController::class,"userSignUp"]);
+
+Route::post("login",[LoginSignupController::class,"userLogin"]);
+
+Route::get("user/{email}", [LoginSignupController::class,"userDetail"]);
