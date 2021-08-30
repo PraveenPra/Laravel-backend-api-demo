0438bbb17399c6ef6f22fe1d07b2f883bf398ca2
Manikanta <prvp6501@gmail.com>
2021-08-30 19:35:30 +0530

    created hospitals model, and controller to perform crud operations

---
+++ app/Http/Controllers/HospitalController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Hospital;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+
+class HospitalController extends Controller
+{
+    public function create(Request $req)
+    {
+        $validator = Validator::make($req->all(), [
+            "name"     => "required",
+            "email"    => "required|email",
+           
+            ]);
+
+        if($validator->fails()) {
+        return response()->json(["status" => "failed", "message" => "validation_error", "errors" => $validator->errors()]);
+        }
+        
+        $result= DB::table('hospitals')->insert([
+            'name'=>$req->name,
+            'email'=>$req->email,
+            'phone'=>$req->phone,
+            'address'=>$req->address
+        ]);
+        if ($result)
+            return response()->json( ["result"=> "added new hospital successfully"]);
+        else
+            return  response()->json( ["result"=> "error updating"]);
+        
+    }
+
+    public function show($id=null)
+    {
+        //
+        $data=DB::table('hospitals')->get();
+        return ['data'=>$data];
+
+    }
+
+    public function edit(Request $req,$id)
+    {
+        dd($id);
+        $data=DB::table('hospitals')->where('id',$id)->update([
+            'name'=>$req->name,
+            'email'=>$req->email,
+            'phone'=>$req->phone,
+            'address'=>$req->address
+        ]);
+        if ($data)return ["result"=> "updated successfully"];
+        else return ["result"=> "failed update"];
+    }
+
+    public function delete($id)
+    {
+    
+        $data=DB::table('hospitals')->where('id',$id)->delete();
+        if ($result)return ["result"=> "delted successfully"];
+        else return ["result"=> "failed to delete"];
+          
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginSignupController;
+use App\Http\Controllers\HospitalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,12 @@ Route::post("signup",[LoginSignupController::class,"userSignUp"]);
 Route::post("login",[LoginSignupController::class,"userLogin"]);
 
 Route::get("user/{email}", [LoginSignupController::class,"userDetail"]);
+
+//route for hospital operations
+Route::post("addhospital",[HospitalController::class,"create"]);
+
+Route::get("showhospital",[HospitalController::class,"show"]);
+
+Route::put("edithospital/{id}",[HospitalController::class,"edit"]);
+
+Route::delete("deletehospital/{id}",[HospitalController::class,"delete"]);
